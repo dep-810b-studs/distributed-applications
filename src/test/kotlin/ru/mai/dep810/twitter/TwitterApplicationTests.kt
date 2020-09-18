ba7301ef01f3e9c7119facf95bf9187e757d6f86
Василий Щербаков <v.sherbakov@softpro.com>
2020-09-18 09:08:48 +0300

    add maven project with dependencies

---
+++ src/test/kotlin/ru/mai/dep810/twitter/TwitterApplicationTests.kt
@@ -0,0 +1,13 @@
+package ru.mai.dep810.twitter
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TwitterApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
