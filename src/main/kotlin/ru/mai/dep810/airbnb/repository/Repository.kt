e15653e990a9a76e3bcb336fec756b8b7899e6f3
sh-vasily <basil-scherbakov@ya.ru>
2020-10-24 19:52:15 +0200

    feature/implementing mongo: repository for clients

---
+++ src/main/kotlin/ru/mai/dep810/airbnb/repository/Repository.kt
@@ -0,0 +1,26 @@
+package ru.mai.dep810.airbnb.repository
+
+import org.springframework.data.mongodb.core.MongoOperations
+import org.springframework.data.mongodb.core.query.Criteria
+import org.springframework.data.mongodb.core.query.Query
+import org.springframework.stereotype.Repository
+import ru.mai.dep810.airbnb.data.*
+import java.util.*
+
+interface IClientRepository{
+    fun findAll() : List<Client>
+    fun save(client: Client): Client
+    fun deleteById(uuid: UUID)
+}
+@Repository("clientRepository")
+class ClientRepository(val mongoOperations: MongoOperations) : IClientRepository {
+    override fun findAll(): List<Client> =
+            mongoOperations.findAll(Client::class.java)
+
+    override fun save(client: Client): Client =
+            mongoOperations.save(client)
+
+    override fun deleteById(uuid: UUID) {
+        mongoOperations.remove(Query.query(Criteria.where("_id").`is`(uuid)),Client::class.java)
+    }
+}
