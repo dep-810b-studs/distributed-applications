b70dde35188385b7b7be980098746cd01a553e8b
Василий Щербаков <v.sherbakov@softpro.com>
2020-11-07 22:35:15 +0300

    feature/implementing_mongo: modify dump clients configuration

--- airbnb-root/server/src/main/kotlin/ru/mai/dep810/airbnb/server/configuration/DumpClientsConfiguration.kt
+++ airbnb-root/server/src/main/kotlin/ru/mai/dep810/airbnb/server/configuration/DumpClientsConfiguration.kt
@@ -1,13 +1,11 @@
 package ru.mai.dep810.airbnb.server.configuration
 
 import org.springframework.boot.context.properties.ConfigurationProperties
-import org.springframework.boot.context.properties.ConstructorBinding
 import org.springframework.context.annotation.Configuration
-import org.springframework.context.annotation.PropertySource
 
 @Configuration
-@ConfigurationProperties(prefix = "dump")
-class DumpConfiguration{
+@ConfigurationProperties(prefix = "dump.clients")
+class DumpClientsConfiguration{
     var needed : Boolean? = null
     var path: String = ""
 }
