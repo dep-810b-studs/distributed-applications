6ce429ac910c64dfb6852076a9ae310fb58aae2a
Василий Щербаков <v.sherbakov@softpro.com>
2020-11-07 22:41:22 +0300

    feature/implementing_mongo: add room controller

---
+++ airbnb-root/server/src/main/kotlin/ru/mai/dep810/airbnb/server/controller/RoomController.kt
@@ -0,0 +1,21 @@
+package ru.mai.dep810.airbnb.server.controller
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.bind.annotation.*
+import ru.mai.dep810.airbnb.server.dto.RoomDto
+import ru.mai.dep810.airbnb.server.service.RoomService
+
+@RestController
+@RequestMapping("/api/v1/rooms")
+class RoomController {
+    @Autowired
+    private lateinit var roomService : RoomService
+
+    @GetMapping
+    fun top3Rooms() : List<RoomDto> =
+            roomService.getTop3Rooms()
+
+    @PostMapping
+    fun addRoom(@RequestBody roomDto: RoomDto) =
+            roomService.addRoom(roomDto)
+}
