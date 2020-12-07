2eaddb0e5eca545d1cae6e508c97381d17b26a1f
Alexander <ledgergap@gmail.com>
2020-12-07 23:10:06 +0300

    clients spark

--- airbnb-root/sparkTransfer/src/main/kotlin/spark/sparkTransfer/optionSpark.kt
+++ airbnb-root/sparkTransfer/src/main/kotlin/spark/sparkTransfer/optionSpark.kt
@@ -4,25 +4,68 @@ import com.mongodb.spark.MongoConnector
 import org.apache.spark.api.java.JavaSparkContext
 import com.mongodb.spark.MongoSpark
 import com.mongodb.spark.config.WriteConfig
+import org.apache.commons.codec.binary.StringUtils
 import org.apache.spark.SparkConf
 import org.apache.spark.SparkContext
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.sql.*
 import org.apache.spark.sql.catalog.Column
 import org.apache.spark.sql.functions.col
-
+import ru.mai.dep810.airbnb.server.data.Room
+import java.io.StringReader
+import org.apache.commons.csv.CSVFormat
+import org.apache.commons.csv.CSVParser
+import org.apache.commons.lang.StringUtils.isNotBlank
+import java.util.*
+import javax.swing.text.Document
 
 
 interface ISparkTransfer{
     fun loadFromCsv(path: String)
+    fun loadFromXml(path:String)
 }
 
 object SparkTransfer : ISparkTransfer {
 
 
-    val columnsToUse = arrayOf("ab", "ba")
+    val columnsAr = arrayOf("name","description","neighborhood_overview","host_location","host_about",
+            "host_neighbourhood",
+            "room_type",
+            "price",
+            "reviews_per_month")
+
+    override fun loadFromXml(path: String) {
+
+        val sparkConf: SparkConf = SparkConf().setAppName("Spark Tags Popularity")
+        sparkConf.setMaster("local[4]")
+        sparkConf.set("spark.mongodb.output.uri","mongodb://localhost:27018/AirBnB.Clients")
+        sparkConf.set("spark.mongodb.output.database","AirBnB")
+        sparkConf.set("spark.mongodb.output.collection","Clients")
+        sparkConf.set("spark.mongodb.output.maxBatchSize","1024")
+
+        val sesBuild = SparkSession.builder().config(sparkConf).orCreate
+
+        val df:JavaRDD<Document> = sesBuild.read()
+                .format("xml")
+                //.option("escape", null)
+                .load(path)
+                .rdd()
+                //.map(XmlUtils::parseXmlToMap)
+                .filter{m -> StringUtils.isNotBlank(m.get(1))}
+                //.map{XmlUtils::unescape}
+                .map{d -> Document(Collections.unmodifiableMap(d))}
+
+
+        MongoSpark.save(df);
+
+
+
+    }
+
+
 
     override fun loadFromCsv(path: String) {
+
 
         val sparkConf: SparkConf = SparkConf().setAppName("Spark Tags Popularity")
             sparkConf.setMaster("local[4]")
@@ -33,25 +76,25 @@ object SparkTransfer : ISparkTransfer {
 
         val sesBuild = SparkSession.builder().config(sparkConf).orCreate
 
-        val df: Dataset<Row> = sesBuild.read()
+        val df:Dataset<Row> = sesBuild.read()
                 .format("csv")
-                .option("header", "true")
-                .option("delimiter", ",")
+                .option("sep","=")
+                .option("header","true")
+                .option("quote", "")
+                .option("escape",null)
+                .option("inferSchema", "true")
+                //.option("escape", null)
                 .load(path)
-                .select(col("name"),
-                        col("description"),
-                        col("neighborhood_overview"),
-                        col("host_location"),
-                        col("host_about"),
-                        col("host_neighbourhood"),
-                        col("room_type"),
-                        col("price"),
-                        col("reviews_per_month"))
+                //.select(*(columnsAr.map { col(it) }.toTypedArray()))
 
 
+        //System.out.println(df.head(3))
+        //MongoSpark.save(df)
+        val temp = df.head(1)
+        System.out.println(temp)
+        val reader = CSVParser((StringReader(temp as String)), CSVFormat.DEFAULT.withHeader())
+        System.out.println(reader)
 
-        System.out.println(df.count())
-        MongoSpark.save(df)
 
 
 
@@ -62,5 +105,6 @@ object SparkTransfer : ISparkTransfer {
 
 
 fun main(args: Array<String>) {
-    val a = SparkTransfer.loadFromCsv("airbnb-root/server/src/main/resources/listings.csv")
+    SparkTransfer.loadFromCsv("airbnb-root/server/src/main/resources/listings.csv")
+    //SparkTransfer.loadFromXml("airbnb-root/server/src/main/resources/Users.xml")
 }
